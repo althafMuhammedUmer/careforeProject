ab0677030d2db76274a521b01d1f5d7b045e7ee2
Althafav <61247025+althafav@users.noreply.github.com>
2022-10-14 16:53:33 +0530

    added star in allowed Host

--- week7/settings.py
+++ week7/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True, cast=bool) #true or false
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

eeedf8afbdf52ae10eef73d23fab60d9d0669153
Althafav <61247025+althafav@users.noreply.github.com>
2022-10-15 18:24:51 +0530

    fixed .env also added static root in settings.py

--- week7/settings.py
+++ week7/settings.py
@@ -143,7 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT =[
+    os.path.join(BASE_DIR , 'staticfiles')
+    
+] 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]

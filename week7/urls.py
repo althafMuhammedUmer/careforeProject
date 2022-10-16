9b719f8294daad201918d03781a34ee536f8b3fc
Althafav <61247025+althafav@users.noreply.github.com>
2022-10-16 13:08:30 +0530

    settings updated also project url updated

--- week7/urls.py
+++ week7/urls.py
@@ -37,4 +37,8 @@ urlpatterns = [
    
 
     
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+# + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

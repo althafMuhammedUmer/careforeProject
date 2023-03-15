a2e984924c374a61957bbbed3bb53573e81e3aeb
Althafav <61247025+althafav@users.noreply.github.com>
2023-03-15 16:05:09 +0530

    fixed category select for admin add product

--- myadmin/forms.py
+++ myadmin/forms.py
@@ -2,7 +2,21 @@
 from email.policy import default
 from django import forms
 from store.models import Product
+from category.models import Category, SubCategory
 
+
+cat_choices = Category.objects.all().values_list('category_name', 'category_name')
+sub_cat_choices = SubCategory.objects.all().values_list('subcategory_name', 'subcategory_name')
+
+cat_choices_list = []
+
+for item in cat_choices:
+    cat_choices_list.append(item)
+    
+    
+sub_cat_choices_list = []
+for item in sub_cat_choices:
+    sub_cat_choices_list.append(item)
 
 
 
@@ -26,10 +40,9 @@ class ProductForm(forms.ModelForm):
             'description': forms.TextInput(attrs={'class': 'form-control'}),
             'price': forms.TextInput(attrs={'class': 'form-control'}),
             'product_discount_price': forms.TextInput(attrs={'class': 'form-control'}),
-          
             'stock': forms.NumberInput(attrs={'class': 'form-control'}),
-            'Category': forms.Select(attrs={'class': 'form-control'}),
-            'subcategorys': forms.Select(attrs={'class': 'form-control'}),
+            'Category': forms.Select(choices=cat_choices_list , attrs={'class': 'form-control'}),
+            'subcategorys': forms.Select(choices=sub_cat_choices_list,  attrs={'class': 'form-control'}),
             
             
           
